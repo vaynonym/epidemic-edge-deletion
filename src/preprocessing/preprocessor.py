f4a75d5358cbb5dfe76e4362c1457cee00645285
Vaynonym <timruschke@googlemail.com>
2020-05-22 18:49:20 +0200

    Loaded the geojson file and filtered information into object DistrictPolygon

--- src/preprocessing/preprocessor.py
+++ src/preprocessing/preprocessor.py
@@ -1,4 +1,6 @@
 import networkx as nx
+import geojson
+
 
 class Preprocessor:
 
@@ -6,5 +8,24 @@ class Preprocessor:
 		pass
 
 	def load_data(self):
+		file = open("data/landkreise-in-germany.geojson", 'r')
+		data_dump = geojson.load(file)
+
+		district_list = [ DistrictPolygon(district.properties["name_2"],
+										  district.properties["name_1"],
+										  district.geometry			    )
+									for district in data_dump["features"]] 
+												
 		graph = nx.Graph()
 		return graph
+	
+
+
+class DistrictPolygon:
+	def __init__(self, district_name, county_name, polygon_representation):
+		self.district_name = district_name
+		self.county_name = county_name
+		self.polygon_representation = polygon_representation
+
+
+
