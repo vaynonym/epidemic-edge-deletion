3ec7d8129d969f783f6588ddba9cd27d74d9eb84
Vaynonym <timruschke@googlemail.com>
2020-05-26 14:42:48 +0200

    A little something for your trouble

--- src/algorithms/main.py
+++ src/algorithms/main.py
@@ -13,11 +13,29 @@ def main():
 	print(" \\__________________________/")
 	print()
 
+	print("  __________________________")
+	print(" /		            \\")
+	print("|        Loading Data...     |")
+	print(" \\__________________________/")
+	print()
+
 	preprocessor = prepro.Preprocessor()
 	graph =  preprocessor.load_data()
+
+	print("  __________________________")
+	print(" /		            \\")
+	print("| Gen. Tree Decomposition... |")
+	print(" \\__________________________/")
+	print()
 	
 	tree_decomposer = td.Tree_Decomposer(graph)
 	nice_tree_decomposition = tree_decomposer.make_nice_tree_decomposition()
+
+	print("  __________________________")
+	print(" /		            \\")
+	print("|   Starting the Algorithm   |")
+	print(" \\__________________________/")
+	print()
 
 	algorithm = algo.Algorithm(nice_tree_decomposition)
 	result = algorithm.execute()	
