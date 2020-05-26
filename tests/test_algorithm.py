ca4d24eaf0df9b911c1ab8e3faef23c08c096894
Vaynonym <timruschke@googlemail.com>
2020-05-26 14:19:26 +0200

    Added tests for generating functions of a partition

--- tests/test_algorithm.py
+++ tests/test_algorithm.py
@@ -16,11 +16,8 @@ def test_generate_partitions_of_bag_of_size():
 
     # act
     result = algo.generate_partitions_of_bag_of_size(bag, max_size)
-    for partition in result:
-        print(partition.blocks)
 
     # assert
-
     for partition1 in result:
         for block1 in partition1.blocks:
             assert len(block1) > 0, "A block should be nonempty" 
@@ -37,7 +34,33 @@ def test_generate_partitions_of_bag_of_size():
             if(not partition1 == partition2):
                 assert not partition1.blocks == partition2.blocks, "Every partition should be unique"
             
+def test_generate_all_functions_of_partition():
+    # assemble
+    algo = alg.Algorithm(nx.Graph())
 
+
+    partition = alg.Partition([[1,2,6,4,5], [6,10,3], [9,7,11,12], [13,14,15,16]])
+    max_size = 7
+    # act
+    result = algo.all_functions_from_partition_to_range(partition, max_size)
+
+    for function1 in result:
+        for block in partition.blocks:
+            assert block in function1.dictionary, "Every block needs to be assigned a value"    
+        for key1 in function1.dictionary:
+            assert function1.dictionary[key1] >= len(key1.node_list), "function(X) >= |X|"
+            assert function1.dictionary[key1] <= max_size, "function(X) <= h"
+        for function2 in result:
+            if(not function1 == function2):
+                functions_are_unique = False
+                for key2 in function2.dictionary:
+                    assert key2 in function1.dictionary, "every function should have the same keys"
+                    if(not function1.dictionary[key2] == function2.dictionary[key2]):
+                        functions_are_unique = True
+                assert functions_are_unique, "Functions should be unique"
+    
+
+    
     
 
 	
